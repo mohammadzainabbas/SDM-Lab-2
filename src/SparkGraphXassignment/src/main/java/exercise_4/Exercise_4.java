1eb642eb74fbec050503a23f0482bfd517fa7fca
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-22 19:41:56 +0200

    Added code

---
+++ src/SparkGraphXassignment/src/main/java/exercise_4/Exercise_4.java
@@ -0,0 +1,24 @@
+package exercise_4;
+
+import com.clearspring.analytics.util.Lists;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.graphframes.GraphFrame;
+
+import java.util.List;
+
+public class Exercise_4 {
+	
+	public static void wikipedia(JavaSparkContext ctx, SQLContext sqlCtx) {
+
+
+	}
+	
+}
