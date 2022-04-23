00b87678cd4cc6c50af04aebb65ad2fad7e4eda9
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-23 13:03:51 +0000

    Minor update

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,22 +1,19 @@
 package utils;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+
+import java.lang.String;
 
 public class Utils {
 
 	static {
 		Logger.getLogger("org.apache.spark").setLevel(Level.WARN);
-		Logger.getLogger("org.apache.spark.storage.BlockManager").setLevel(
-				Level.ERROR);
+		Logger.getLogger("org.apache.spark.storage.BlockManager").setLevel(Level.ERROR);
+	}
+
+    public static void print(String text) {
+		System.out.println(text);
 	}
 
 }
