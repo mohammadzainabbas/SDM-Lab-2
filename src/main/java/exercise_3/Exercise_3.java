71dcaa6d430270360c2698e0369a6a40a6bddd67
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-04-23 15:23:52 +0000

    Work in progress for exercise 3

--- src/main/java/exercise_3/Exercise_3.java
+++ src/main/java/exercise_3/Exercise_3.java
@@ -74,7 +74,73 @@ public class Exercise_3 {
     }
 
     public static void shortestPathsExt(JavaSparkContext ctx) {
+        Utils.log("Create labels, vertices and edges");
+        Map<Long, String> labels = ImmutableMap.<Long, String>builder()
+                .put(1l, "A")
+                .put(2l, "B")
+                .put(3l, "C")
+                .put(4l, "D")
+                .put(5l, "E")
+                .put(6l, "F")
+                .build();
 
+        List<Tuple2<Object,Integer>> vertices = Lists.newArrayList(
+                new Tuple2<Object,Integer>(1l,0),
+                new Tuple2<Object,Integer>(2l,Integer.MAX_VALUE),
+                new Tuple2<Object,Integer>(3l,Integer.MAX_VALUE),
+                new Tuple2<Object,Integer>(4l,Integer.MAX_VALUE),
+                new Tuple2<Object,Integer>(5l,Integer.MAX_VALUE),
+                new Tuple2<Object,Integer>(6l,Integer.MAX_VALUE)
+        );
+        List<Edge<Integer>> edges = Lists.newArrayList(
+                new Edge<Integer>(1l,2l, 4), // A --> B (4)
+                new Edge<Integer>(1l,3l, 2), // A --> C (2)
+                new Edge<Integer>(2l,3l, 5), // B --> C (5)
+                new Edge<Integer>(2l,4l, 10), // B --> D (10)
+                new Edge<Integer>(3l,5l, 3), // C --> E (3)
+                new Edge<Integer>(5l, 4l, 4), // E --> D (4)
+                new Edge<Integer>(4l, 6l, 11) // D --> F (11)
+        );
+
+        Utils.log("Create RDD for vertices and edges");
+        JavaRDD<Tuple2<Object,Integer>> verticesRDD = ctx.parallelize(vertices);
+        JavaRDD<Edge<Integer>> edgesRDD = ctx.parallelize(edges);
+
+        Utils.log("Create Graph from vertices and edges");
+        Graph<Integer,Integer> G = Graph.apply(verticesRDD.rdd(),edgesRDD.rdd(),1, StorageLevel.MEMORY_ONLY(), StorageLevel.MEMORY_ONLY(),
+                scala.reflect.ClassTag$.MODULE$.apply(Integer.class),scala.reflect.ClassTag$.MODULE$.apply(Integer.class));
+
+        GraphOps ops = new GraphOps(G, scala.reflect.ClassTag$.MODULE$.apply(Integer.class),scala.reflect.ClassTag$.MODULE$.apply(Integer.class));
+
+        String srcLabel = labels.get(1l);
+
+        Utils.log("Run pregel over our graph with apply, scatter and gather functions");
+        Utils.line_separator();
+        
+        JavaRDD<Tuple2<Object,Integer>> output_rdd = ops.pregel(
+            INITIAL_VALUE,
+            Integer.MAX_VALUE,
+            EdgeDirection.Out(),
+            new VProg(),
+            new sendMsg(),
+            new merge(),
+            ClassTag$.MODULE$.apply(Integer.class))
+            .vertices()
+            .toJavaRDD();
+            
+        Utils.line_separator();
+        
+        output_rdd
+            .sortBy(f -> ((Tuple2<Object, Integer>) f)._1, true, 0)
+            .foreach(v -> {
+
+                Tuple2<Object,Integer> vertex = (Tuple2<Object,Integer>) v;
+                Long vertexId = (Long) vertex._1;
+                Integer cost = (Integer) vertex._2;
+                String descLabel = labels.get(vertexId);
+
+                Utils.print("Minimum cost to get from '" + srcLabel + "' to '" + descLabel + "' is " + cost);
+            });
     }
 	
 }
